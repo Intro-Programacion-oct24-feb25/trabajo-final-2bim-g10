75610d51dae27e4c96a4dc25681b244f6f621367
Luis <luiseduardosa2006@gmail.com>
2025-01-23 11:10:34 -0500

    v0

---
+++ proyecto_java/JavaFinal/src/javafinal/JavaFinal.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javafinal;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author utpl
+ */
+public class JavaFinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int opcion;
+        String[] actividades = {"Fútbol", "Natación", "Atletismo", "Básquetbol", "Ciclismo", "Tenis", "Yoga"};
+        String[] participantes = new String[7];
+        int actividadesRegistradas = 0;
+        String respuesta = "";
+        boolean bandera = true;
+        while (bandera) {
+            System.out.println("Las");
+
+        }
+    }
+
+}
